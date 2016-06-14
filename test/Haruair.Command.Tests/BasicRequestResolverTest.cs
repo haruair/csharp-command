eaf720ad0b02616241c5e929d22d306fb643398d
Edward Kim <onward.edkim@gmail.com>
2016-06-14 21:39:26 +1000

    add resolver test for basic request

--- test/Haruair.Command.Tests/BasicRequestResolverTest.cs
+++ test/Haruair.Command.Tests/BasicRequestResolverTest.cs
@@ -50,6 +50,46 @@ namespace Haruair.Command.Tests
 			Assert.AreEqual ("hello", request.Command);
 			Assert.AreEqual ("world", request.Method);
 		}
+
+		[Test ()]
+		public void EssentialExpressionTestCase ()
+		{
+			var args = new string[] { "hello", "world", "--url", "http://haruair.com", "edward" };
+			var request = resolver.Resolve (args);
+
+			Assert.AreEqual ("hello", request.Command);
+			Assert.AreEqual ("world", request.Method);
+			Assert.IsTrue (request.Options.ContainsKey ("url"));
+
+			string url;
+
+			Assert.IsTrue (request.Options.TryGetValue ("url", out url));
+			Assert.AreEqual ("http://haruair.com", url);
+
+			Assert.IsTrue (request.Params.Contains ("edward"));
+		}
+
+		[Test ()]
+		public void FullExpressionTestCase ()
+		{
+			var args = new string[] {
+				"hello", "world", "--url", "http://haruair.com", "edward", "--nick-name", "eddy", "SecondParam" };
+			var request = resolver.Resolve (args);
+
+			Assert.AreEqual ("hello", request.Command);
+			Assert.AreEqual ("world", request.Method);
+			Assert.IsTrue (request.Options.ContainsKey ("url"));
+
+			string url, name;
+
+			Assert.IsTrue (request.Options.TryGetValue ("url", out url));
+			Assert.AreEqual ("http://haruair.com", url);
+
+			Assert.IsTrue (request.Options.TryGetValue ("nick-name", out name));
+			Assert.AreEqual ("eddy", name);
+
+			Assert.IsTrue (request.Params.Contains ("edward"));
+			Assert.IsTrue (request.Params.Contains ("SecondParam"));
+		}
 	}
 }
-
